8415f412ca1fe2c097b7087d05ae540dd4100da5
HysteriaDev <35158775+HysteriaDev@users.noreply.github.com>
2018-01-12 05:21:47 -0600

    Update Core.php

--- src/Hysteria/Core.php
+++ src/Hysteria/Core.php
@@ -6,3 +6,12 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as C;
 
 class Core extends PluginBase {
+    
+    const PERM_RANK = C::BOLD . C::RED . "Permission> " . C::RESET . C::GRAY . "You Need A Rank To Access This Command";
+    const PERM_STAFF = C::BOLD . C::RED . "Permission> " . C::RESET . C::GRAY . "Only Staff Can Use This Command";
+    const USE_IN_GAME = C::BOLD . C::RED . "Command> " . C::RESET . C::GRAY . "Use Command In Game";
+    
+    public function onEnable() {
+        $this->getLogger()->info(C::BOLD . C::RED"Enabled!");
+        
+    //More To Be Done//
